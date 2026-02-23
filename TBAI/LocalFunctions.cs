fb95ec0a39b85dad64d19ed50f7816402f5b4e2e
JCFAX <jcferrero@axalphaconsulting.com>
2026-02-23 01:04:28 +0100

    Intial

---
+++ TBAI/LocalFunctions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Common;
+using System.Runtime;
+using System.Data;
+using System.Data.SqlClient;
+using System.Globalization;
+using System.Collections.Generic;
+
+namespace Profarmas
+{
+    /// <summary> Funciones locales </summary>
+    public class LocalFunctions
+    {
+
+    }
+}
